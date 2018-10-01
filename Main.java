03f8fa94b9a34b44f7c06fdedcf1d50094b2faaf
Antoine Jean <antoinejeanmec@hotmail.fr>
2018-10-01 21:03:46 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		// mastermind construction
+		Mastermind mastermind = new Mastermind();
+		
+		// generating code
+		String[] code = mastermind.genCode();
+		
+		// some var
+		Integer compt = 1;
+		Integer victoire = 0;
+		Integer defaite = 0;
+		
+		// game loop
+		do {
+			
+			System.out.println("Code is : " + String.join("", code));
+			System.out.println("Essai n° " + compt);
+			
+			String[] saisie = mastermind.recup_saisie();
+			
+			System.out.println(String.join(" ", saisie));
+			
+			String[] verif = mastermind.verif_code(code, saisie);
+			
+			if (String.join("", verif).equals("VVVV"))
+				victoire = 1;
+			if (compt == 8)
+				defaite = 1;
+			
+			compt++;
+			
+		} while (victoire == 0 && defaite == 0);
+		
+		mastermind.sc.close();
+		
+		if (victoire == 1)
+			System.out.println("gagné en " + compt + " coups");
+		else if (defaite == 1)
+			System.out.println("perdu");
+
+	}
+
+}
---
+++ Mastermind.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Mastermind {
+	
+	public Scanner sc;
+	private String[] verif;
+
+	public Mastermind() {
+		super();
+		
+		sc = new Scanner(System.in);
+		this.verif = new String[4];
+	}
+	
+	public String[] genCode() {
+		
+		Random rand = new Random();
+		Integer nb_alea = rand.nextInt(9999);
+		String str_alea = String.format("%04d", nb_alea);		
+		
+		return str_alea.split("");
+	}
+	
+	public String[] recup_saisie() {
+		
+		String saisie = sc.nextLine();
+		
+		return saisie.split("");
+	}
+	
+	public String[] verif_code(String[] code, String[] saisie) {
+		
+		Integer index = 0;
+		
+		for (String s : saisie) {
+			if (s.equals(code[index]) == true) {
+				verif[index] = "V";
+			} else if (Arrays.asList(code).contains(s) == true) {
+				verif[index] = "P";
+			} else if (Arrays.asList(code).contains(s) == false) {
+				verif[index] = "X";
+			}
+			
+			index++;
+		}
+		
+		for (int i = 0; i < verif.length; i++) {
+			System.out.print(verif[i] + " ");
+		}
+		
+		System.out.println(" ");
+		
+		return verif;
+	}
+}
